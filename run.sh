7ca49c16df3e0444b2ab2890c5ccd1b84bc8c457
Irvan Rahmadi <mirvanrahmadi@gmail.com>
2024-03-28 15:17:39 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e44c8dc0-90dd-42b6-b6c7-e590f75a2973/)
 
 
 echo "$curl_output" >> Update.tsx
